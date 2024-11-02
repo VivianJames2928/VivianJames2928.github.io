7eb4b1df50ea036bcf40ba708909e12e56e26965
Vivian James <vivianjames2928@gmail.com>
2024-11-02 10:14:07 +0100

    Imomotimi Dance Contest

---
+++ imomotimidance/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./*.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
